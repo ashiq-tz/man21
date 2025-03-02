850f6a8edb8d1a2b40277a18c0944e08d9bbc134
ashiq <ashiq@ashiqs-MacBook-Air.local>
2025-03-02 10:22:05 +0530

    view engine setup

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const env = require("dotenv").config();
 
 const db = require("./config/db");
-db()
+const userRouter = require("./routes/userRouter");
+db();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')]);
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.use("/",userRouter);
 
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 
-app.listen(process.env.PORT, ()=>{
-    console.log("Server Running");
-})
+  
 
 
 module.exports = app;
---
+++ controllers/user/userController.js
@@ -0,0 +1,27 @@
+const loadHomePage = async (req,res) =>{
+    try {
+        
+        return res.render("signup");
+
+    } catch (error) {
+
+        console.log("home page not found");
+        res.status(500).send("server error")
+        
+    }
+}
+
+const pageNotFound = async (req,res) =>{
+    try {
+        res.render("page-404")
+    } catch (error) {
+        res.redirect("/pageNotFound")
+    }
+}
+
+
+
+module.exports = {
+    loadHomePage,
+    pageNotFound
+}
