faf6bed4618287dd792f3fafa996c3e1c7347b37
ashiq <ashiq@ashiqs-MacBook-Air.local>
2025-03-14 02:18:49 +0530

    forgot password

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router();
 const userController = require("../controllers/user/userController");
 const passport = require("../config/passport");
+const profileController = require("../controllers/user/profileController")
 
 
 router.get("/",userController.loadHomePage);
@@ -26,6 +27,16 @@ router.post('/login',userController.login);
 
 router.get('/logout',userController.logout);
 
+//Profile Management
+router.get("/forgot-password",profileController.getForgotPassPage)
+router.post("/forgot-email-valid",profileController.forgotEmailValid)
+
+router.post("/verify-passForgot-otp",profileController.verifyForgotPassOtp);
+router.get("/reset-password",profileController.getResetPassPage)
+
+router.post("/resend-forgot-otp",profileController.resendOtp);
+
+router.post("/reset-password",profileController.postNewPassword);
 
 
 
